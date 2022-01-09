1051d4e9d1ebba68b984f411bdab74e8a56ad77f
Connor Adams <connorads@users.noreply.github.com>
2022-01-09 12:08:22 +0000

    Allow tree-shaking of three library

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,33 @@
-import * as THREE from "three";
+import {
+  Scene,
+  WebGLRenderer,
+  DoubleSide,
+  MeshPhongMaterial,
+  Geometry,
+  Vector3,
+  Face3,
+  FaceColors,
+  Mesh,
+  AmbientLight,
+  PointLight,
+  PerspectiveCamera,
+} from "three";
 import WAVES from "vanta/dist/vanta.waves.min";
+
+const THREE = {
+  Scene,
+  WebGLRenderer,
+  DoubleSide,
+  MeshPhongMaterial,
+  Geometry,
+  Vector3,
+  Face3,
+  FaceColors,
+  Mesh,
+  AmbientLight,
+  PointLight,
+  PerspectiveCamera,
+};
 
 const emerald700 = 0x47857;
 const target = document.body;
