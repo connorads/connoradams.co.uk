d3f5638c55bbce02c34e5c6d849f791d5b4a969e
Connor Adams <connorads@users.noreply.github.com>
2022-01-23 18:09:18 +0000

    Add type to request

--- functions/api/hello.ts
+++ functions/api/hello.ts
@@ -1,15 +1,7 @@
-export async function onRequestPost(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
+export async function onRequestPost(context: { request: Request }) {
+  const { request } = context;
 
-  const json = await request.json()
+  const json = await request.json();
   console.log(JSON.stringify(json, null, 2));
   return new Response(JSON.stringify(json));
 }
