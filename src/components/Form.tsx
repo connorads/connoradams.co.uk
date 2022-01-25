e410bc6bedbd467327ea63d86d2df057ca036105
Connor Adams <connorads@users.noreply.github.com>
2022-01-25 18:24:37 +0000

    Adjust validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,9 +3,9 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from "zod";
 
 export const formSchema = z.object({
-  name: z.string().min(5),
+  name: z.string().min(2),
   email: z.string().email(),
-  message: z.string().min(20),
+  message: z.string().min(15),
 });
 
 type FormSubmission = z.infer<typeof formSchema>;
