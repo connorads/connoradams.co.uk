bf3fbadc3775792ff9fb4a5c6c7135ec65139b0b
Connor Adams <connorads@users.noreply.github.com>
2022-01-30 17:48:36 +0000

    Add Sentry to backend functions (#4)

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,29 @@
+import Toucan from "toucan-js";
+
 const errorHandler = async ({
+  request,
+  waitUntil,
   next,
+  env,
 }: {
+  request: Request;
+  waitUntil: (promise: Promise<any>) => void;
   next: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  env: { SENTRY_DSN?: string; ENVIRONMENT?: string };
 }) => {
+  let sentry: Toucan | undefined;
+  if (env.SENTRY_DSN)
+    sentry = new Toucan({
+      dsn: env.SENTRY_DSN,
+      environment: env.ENVIRONMENT || "development",
+      context: { waitUntil, request },
+      allowedHeaders: ["user-agent"],
+      allowedSearchParams: /(.*)/,
+    });
   try {
     return await next();
   } catch (err) {
+    if (sentry) sentry.captureException(err);
     return new Response("Internal Server Error", { status: 500 });
   }
 };
